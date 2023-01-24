bf22254236a2b59c00531b5776dd71f546fd7e13
Lowell Belany <lrbelany@gmail.com>
2023-01-24 15:36:06 -0800

    boot project connected and online

---
+++ BattleLog/src/main/java/com/skilldistillery/battle/BattleLogApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.battle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattleLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattleLogApplication.class, args);
+	}
+
+}
